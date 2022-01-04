23bffa36cfc129785b57db70b508c7d325212057
Rafael Birkmann <mail@rafaelbirkmann.de>
2022-01-04 13:44:39 +0100

    scrollbar

--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -74,8 +74,11 @@ const Navbar = () => {
             </nav>
           </div>
           <Searchbar />
-          <div className="z-10 hidden ml-6 lg:flex">
-            <a href="#" className="p-2 rounded-full hover:bg-gray-200">
+          <div className="z-10 flex ml-6 space-x-2">
+            <a
+              href="#"
+              className="hidden p-2 rounded-full hover:bg-gray-200 lg:block"
+            >
               <svg width="24px" height="24px" fill="#111" viewBox="0 0 24 24">
                 <path d="M21.11 4a6.6 6.6 0 0 0-4.79-1.92A6.27 6.27 0 0 0 12 3.84 6.57 6.57 0 0 0 2.89 4c-2.8 2.68-2.45 7.3.88 10.76l6.84 6.63A2 2 0 0 0 12 22a2 2 0 0 0 1.37-.54l.2-.19.61-.57c.6-.57 1.42-1.37 2.49-2.41l2.44-2.39 1.09-1.07c3.38-3.55 3.8-8.1.91-10.83zm-2.35 9.4l-.25.24-.8.79-2.44 2.39c-1 1-1.84 1.79-2.44 2.36L12 20l-6.83-6.68c-2.56-2.66-2.86-6-.88-7.92a4.52 4.52 0 0 1 6.4 0l.09.08a2.12 2.12 0 0 1 .32.3l.9.94.9-.94.28-.27.11-.09a4.52 4.52 0 0 1 6.43 0c1.97 1.9 1.67 5.25-.96 7.98z"></path>
               </svg>
@@ -86,6 +89,13 @@ const Navbar = () => {
                 <path d="M20 5H4a2 2 0 0 0-2 2v13a3 3 0 0 0 3 3h14a3 3 0 0 0 3-3V7a2 2 0 0 0-2-2zm0 15a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V7h16z"></path>
               </svg>
             </a>
+            <div className="lg:hidden">
+              <button className="p-2 rounded-full hover:bg-gray-200">
+                <svg width="24px" height="24px" fill="#111" viewBox="0 0 24 24">
+                  <path d="M21 13H3c-.6 0-1-.4-1-1s.4-1 1-1h18c.6 0 1 .4 1 1s-.4 1-1 1zm0-8H3c-.6 0-1-.4-1-1s.4-1 1-1h18c.6 0 1 .4 1 1s-.4 1-1 1zm0 16H3c-.6 0-1-.4-1-1s.4-1 1-1h18c.6 0 1 .4 1 1s-.4 1-1 1z"></path>
+                </svg>
+              </button>
+            </div>
           </div>
         </div>
       </Container>
--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,7 @@ const Home: NextPage = () => {
         </div>
         <div className="my-20">
           <h2 className="mb-8 text-2xl">Die richtige Ausrüstung</h2>
-          <div className="flex gap-4 overflow-x-scroll flex-nowrap ">
+          <div className="flex gap-4 overflow-x-scroll flex-nowrap hover:scrollbar-thumb-gray-900 scrollbar-thin">
             <div className="flex-none w-3/4 lg:w-1/3">
               <img
                 src="https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.0/w_525,c_limit/eaad3abc-94cb-442a-8a22-9b006153eb80/react-miler-2-shield-wetterfester-strassenlaufschuh-fur-WnSDPL.png"
