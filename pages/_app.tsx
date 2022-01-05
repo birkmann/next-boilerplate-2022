119673a495dd2bc3e1b119c5f15b46c30a2b7c42
Rafael Birkmann <mail@rafaelbirkmann.de>
2022-01-05 17:12:41 +0100

    viewport

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { MobileMenuContext } from '@context/MobileMenu.context'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <MobileMenuContext>
-      <Component {...pageProps} />
-    </MobileMenuContext>
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+        />
+      </Head>
+      <MobileMenuContext>
+        <Component {...pageProps} />
+      </MobileMenuContext>
+    </>
   )
 }
 
