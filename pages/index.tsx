93740c96ecea04b13bfbff878676bd9e06e77c13
Rafael Birkmann <mail@rafaelbirkmann.de>
2022-01-19 12:31:26 +0100

    images

--- pages/index.tsx
+++ pages/index.tsx
@@ -257,80 +257,139 @@ const Home: NextPage = () => {
       </div>
       <div className="my-20">
         <Container>
-          <h2 className="mb-8 text-2xl">Die richtige Ausrüstung</h2>
+          <h2 className="mb-8 text-2xl">Aktuelle Trends</h2>
         </Container>
-        <Container>
-          <div className="flex gap-4 overflow-x-scroll flex-nowrap hover:scrollbar-thumb-gray-900 scrollbar-thin">
-            <div className="flex-none w-3/4 lg:w-1/3">
+        <div className="max-w-[1800px] mx-auto lg:px-12">
+          <div className="flex w-full overflow-x-auto slider snap-x hover:scrollbar-thumb-gray-900 scrollbar-thin">
+            <div className="w-3/4 mr-3 snap-start first:-ml-0 last:mr-0 shrink-0 md:w-[33%]">
               <Image
-                src="https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.0/w_525,c_limit/eaad3abc-94cb-442a-8a22-9b006153eb80/react-miler-2-shield-wetterfester-strassenlaufschuh-fur-WnSDPL.png"
+                src="https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.0/h_543,c_limit/c00bc3e4-4a4a-4bb2-a45b-9add35d1404f/air-force-1-07-herrenschuh-kdPWF0.png"
                 alt="Nike"
                 width={591}
                 height={591}
               />
               <div className="justify-between mt-4 mb-8 lg:flex">
                 <div>
-                  <h4>Nike React Miler 2 Shield</h4>
-                  <p className="text-gray-500">
-                    Wetterfester Straßenlaufschuh für Damen
-                  </p>
+                  <h4>Nike Air Force 1 07</h4>
+                  <p className="text-gray-500">Herrenschuh</p>
                 </div>
-                <p className="mr-4 whitespace-nowrap">139,99 €</p>
+                <p className="mr-4 whitespace-nowrap">119,99 €</p>
               </div>
             </div>
-            <div className="flex-none w-3/4 lg:w-1/3">
+            <div className="w-3/4 mr-3 snap-start first:-ml-0 md:last:mr-0 shrink-0 md:w-[33%]">
               <Image
-                src="https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.0/w_525,c_limit/ed177def-b551-43a8-95fa-40353d8fb84f/pegasus-trail-3-gore-tex-wasserfester-trail-laufschuh-fur-herren-FngTl6.png"
+                src="https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.0/h_543,c_limit/358b5ffe-0704-4c5a-89fd-0fa5b2b1f99b/air-max-plus-iii-herrenschuh-3BSBtx.png"
                 alt="Nike"
                 width={591}
                 height={591}
               />
               <div className="justify-between mt-4 mb-8 lg:flex">
                 <div>
-                  <h4>Nike Pegasus Trail 3 GORE-TEX</h4>
-                  <p className="text-gray-500">
-                    Wasserfester Trail-Laufschuh für Herren
-                  </p>
+                  <h4>Nike Air Force 1 07</h4>
+                  <p className="text-gray-500">Herrenschuh</p>
                 </div>
-                <p className="mr-4 whitespace-nowrap">149,99 €</p>
+                <p className="mr-4 whitespace-nowrap">119,99 €</p>
               </div>
             </div>
-            <div className="flex-none w-3/4 lg:w-1/3">
+            <div className="w-3/4 mr-3 snap-start first:-ml-0 md:last:mr-0 shrink-0 md:w-[33%]">
               <Image
-                src="https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.0/w_591,c_limit/a05f509e-8d66-411e-9ba1-38b64f8bd559/air-zoom-pegasus-38-shield-wetterfester-strassenlaufschuh-fur-herren-BKgmbZ.png"
+                src="https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.0/h_543,c_limit/3bb13a3e-4622-44c6-a864-3a3eb1ae430d/air-force-1-07-herrenschuh-kdPWF0.png"
                 alt="Nike"
                 width={591}
                 height={591}
               />
               <div className="justify-between mt-4 mb-8 lg:flex">
                 <div>
-                  <h4>Nike React Miler 2 Shield</h4>
-                  <p className="text-gray-500">
-                    Wetterfester Straßenlaufschuh für Damen
-                  </p>
+                  <h4>Nike Air Force 1 07</h4>
+                  <p className="text-gray-500">Herrenschuh</p>
                 </div>
-                <p className="mr-4 whitespace-nowrap">139,99 €</p>
+                <p className="mr-4 whitespace-nowrap">119,99 €</p>
               </div>
             </div>
-            <div className="flex-none w-3/4 lg:w-1/3">
+            <div className="w-3/4 mr-3 snap-start first:-ml-0 md:last:mr-0 shrink-0 md:w-[33%]">
               <Image
-                src="https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.0/w_591,c_limit/aaf5068f-f45c-4492-898b-100b463107ed/air-zoom-pegasus-37-shield-herren-laufschuh-nf5kSS.png"
+                src="https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.0/w_524,c_limit/3dcd2a33-60e2-4eee-8376-f9cce28adff0/superrep-go-2-trainingsschuh-zHbNF4.png"
                 alt="Nike"
                 width={591}
                 height={591}
               />
               <div className="justify-between mt-4 mb-8 lg:flex">
                 <div>
-                  <h4>Nike Pegasus Trail 3 GORE-TEX</h4>
+                  <h4>Nike SuperRep Go 2</h4>
+                  <p className="text-gray-500">Damen-Trainingsschuh</p>
+                </div>
+                <p className="mr-4 whitespace-nowrap">99,99 €</p>
+              </div>
+            </div>
+            <div className="w-3/4 mr-3 snap-start first:-ml-0 md:last:mr-0 shrink-0 md:w-[33%]">
+              <Image
+                src="https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.0/w_524,c_limit/bb6f2759-23f5-4aec-8ae8-246a7a741f36/dri-fit-alpha-gepolsterter-sport-bh-mit-reissverschluss-vorne-und-starkem-halt-fur-8x39Kh.png"
+                alt="Nike"
+                width={591}
+                height={591}
+              />
+              <div className="justify-between mt-4 mb-8 lg:flex">
+                <div>
+                  <h4>Nike Dri-FIT Alpha</h4>
                   <p className="text-gray-500">
-                    Wetterfester Straßenlaufschuh für Damen
+                    Gepolsterter Sport-BH mit Reißverschluss vorne und starkem
+                    Halt für Damen
                   </p>
                 </div>
-                <p className="mr-4 whitespace-nowrap">149,99 €</p>
+                <p className="mr-4 whitespace-nowrap">59,99 €</p>
+              </div>
+            </div>
+            <div className="w-3/4 mr-3 snap-start first:-ml-0 md:last:mr-0 shrink-0 md:w-[33%]">
+              <Image
+                src="https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.0/w_524,c_limit/deb52d93-950c-49d3-8da0-2a9f4e24e999/dri-fit-one-luxe-7-8-leggings-mit-mittelhohem-bund-fur-Z5Kms0.png"
+                alt="Nike"
+                width={591}
+                height={591}
+              />
+              <div className="justify-between mt-4 mb-8 lg:flex">
+                <div>
+                  <h4>Nike Dri-FIT One Luxe</h4>
+                  <p className="text-gray-500">
+                    7/8-Leggings mit mittelhohem Bund für Damen
+                  </p>
+                </div>
+                <p className="mr-4 whitespace-nowrap">94,99 €</p>
+              </div>
+            </div>
+            <div className="w-3/4 mr-3 snap-start first:-ml-0 md:last:mr-0 shrink-0 md:w-[33%]">
+              <Image
+                src="https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.0/w_524,c_limit/d3d03f43-abe4-4275-bbdc-519a1fe5e64c/dri-fit-one-luxe-tanktop-mit-schmaler-passform-fur-sq08rW.png"
+                alt="Nike"
+                width={591}
+                height={591}
+              />
+              <div className="justify-between mt-4 mb-8 lg:flex">
+                <div>
+                  <h4>Nike Dri-FIT One Luxe</h4>
+                  <p className="text-gray-500">
+                    Tankop in schmaler Passform für Damen
+                  </p>
+                </div>
+                <p className="mr-4 whitespace-nowrap">44,99 €</p>
+              </div>
+            </div>
+            <div className="w-3/4 mr-3 snap-start first:-ml-0 md:last:mr-0 shrink-0 md:w-[33%]">
+              <Image
+                src="https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_1.0/w_524,c_limit/3dcd2a33-60e2-4eee-8376-f9cce28adff0/superrep-go-2-trainingsschuh-zHbNF4.png"
+                alt="Nike"
+                width={591}
+                height={591}
+              />
+              <div className="justify-between mt-4 mb-8 lg:flex">
+                <div>
+                  <h4>Nike SuperRep Go 2</h4>
+                  <p className="text-gray-500">Damen-Trainingsschuh</p>
+                </div>
+                <p className="mr-4 whitespace-nowrap">99,99 €</p>
               </div>
             </div>
           </div>
-        </Container>
+        </div>
       </div>
       <Footer />
     </div>
